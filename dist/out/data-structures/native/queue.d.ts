958d3f4d7e8a4666a4f714b3297b3d73cfcbdb92
Matt Ashpole <m.d.ashpole@gmail.com>
2024-11-25 16:11:06 +0000

    more

--- dist/out/data-structures/native/queue.d.ts
+++ dist/out/data-structures/native/queue.d.ts
@@ -1,14 +1,14 @@
 export declare namespace Queue {
-	interface Queue<T> {
+	interface QueueStruct<T> {
 		data: T[];
 		front: number;
 		rear: number;
 	}
 
-	function New<T>(): Queue<T>;
-	function Enqueue<T>(data: Queue<T>, item: T): void;
-	function Dequeue<T>(data: Queue<T>): T | undefined;
-	function Peek<T>(data: Queue<T>): T;
-	function IsEmpty<T>(data: Queue<T>): boolean;
-	function Size<T>(data: Queue<T>): number;
+	function New<T>(): QueueStruct<T>;
+	function Enqueue<T>(data: QueueStruct<T>, item: T): void;
+	function Dequeue<T>(data: QueueStruct<T>): T | undefined;
+	function Peek<T>(data: QueueStruct<T>): T;
+	function IsEmpty<T>(data: QueueStruct<T>): boolean;
+	function Size<T>(data: QueueStruct<T>): number;
 }
